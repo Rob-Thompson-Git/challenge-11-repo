20828a5af0f57d7d5f20cebcac79160c0c3fc847
RobThompson <rob.thomp15@gmail.com>
2022-09-29 17:05:47 -0500

    revamped the server.js file and added htmlRoute.js and apiRoute.js (with Erik Hirsh)

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use('/', htmlRoutes);
 
-app.get('/api/terms', (req, res) => res.json(termData));
+// app.get('/api/terms', (req, res) => res.json(termData));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
+
+//read the file
+  //parse into actual js object
+//add new note to the object (db object)
+  //turn object back into string and writeToFile back into db.json
+  //note: everytime we need the front end and backend to communicate, we need api request
